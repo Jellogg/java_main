1bd95ae12424405db778f175a2b0aba9f9bc4a07
chellog <jinseo0918@gmail.com>
2023-06-19 16:40:18 +0900

    53Day

---
+++ java-main/servlet/src/main/java/xyz/itwill/servlet/DBCPServlet.java
@@ -0,0 +1,24 @@
+package xyz.itwill.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DBCPServlet
+ */
+@WebServlet("/dbcp.itwill")
+public class DBCPServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
