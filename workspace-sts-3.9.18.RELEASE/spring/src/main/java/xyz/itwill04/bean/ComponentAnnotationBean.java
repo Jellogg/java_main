b9e32e4bf1ff3e7766fedaae90adef955ad91160
chellog <jinseo0918@gmail.com>
2023-07-26 20:17:58 +0900

    79Day

---
+++ workspace-sts-3.9.18.RELEASE/spring/src/main/java/xyz/itwill04/bean/ComponentAnnotationBean.java
@@ -0,0 +1,5 @@
+package xyz.itwill04.bean;
+
+public class ComponentAnnotationBean {
+
+}
---
+++ workspace-sts-3.9.18.RELEASE/spring/src/main/java/xyz/itwill05/di/Student.java
@@ -0,0 +1,14 @@
+package xyz.itwill05.di;
+
+public class Student {
+	private int num;
+	private String name;
+	private String email;
+	
+	public Student() {
+		System.out.println("");
+		
+		toString
+	}
+
+}
